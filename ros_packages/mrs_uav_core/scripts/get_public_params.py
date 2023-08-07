2d208a7636561e88f61390353a6d02fa0bbf56c7
Tomas Baca <klaxalk@gmail.com>
2023-08-07 09:39:54 +0200

    added the param getter scripts

---
+++ ros_packages/mrs_uav_core/scripts/get_public_params.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import rospkg
+import os
+import roslaunch
+import subprocess
+
+class ParamsGetter:
+
+    def __init__(self):
+
+        rospack = rospkg.RosPack()
+
+        packages = [
+            "mrs_uav_trackers",
+            "mrs_uav_controllers",
+            "mrs_uav_managers",
+            "mrs_uav_trajectory_generation",
+        ]
+
+        for package in packages:
+
+            s = subprocess.check_output("rosrun {} get_public_params.py".format(package), shell = True)
+            print(s.decode("utf-8"))
+
+if __name__ == '__main__':
+    params_getter = ParamsGetter()
