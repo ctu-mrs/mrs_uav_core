df7a857df978b02f76519a79c8f5099e58e310d1
Tomas Baca <klaxalk@gmail.com>
2024-02-11 11:23:37 +0100

    updated test scripts

--- test/compile_tests.sh
+++ test/compile_tests.sh
@@ -5,8 +5,6 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
-# build the packages
+# build the package
 catkin build -DMRS_ENABLE_TESTING=1 # it has to be fully built normally before building with --catkin-make-args tests
 catkin build -DMRS_ENABLE_TESTING=1 --catkin-make-args tests
-
-catkin test -i -p 1 -s -c
