24836297572bc20767844ec2f7305699e864727d
Tomas Baca <Tomas Baca>
2026-01-23 14:51:44 +0100

    updated gitman branches

--- ros_packages/mrs_uav_core/launch/core.launch.py
+++ ros_packages/mrs_uav_core/launch/core.launch.py
@@ -197,6 +197,7 @@ def generate_launch_description():
             {'use_sim_time': use_sim_time},
         ],
         # prefix=[debug],
+        # prefix="valgrind --tool=memcheck --leak-check=no --track-origins=no --show-reachable=no --errors-for-leak-kinds=definite --num-callers=12",
         condition=UnlessCondition(standalone)
     )
 
